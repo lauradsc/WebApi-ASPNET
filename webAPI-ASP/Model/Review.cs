87d24658d22c06a8ac90279845ae11909da6d9c0
LAURA DE SOUZA COSTA <p811916@d02.prodemge.gov.br>
2023-11-29 11:39:12 -0300

    add: important

--- webAPI-ASP/Model/Review.cs
+++ webAPI-ASP/Model/Review.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public int Title { get; set; }
+        public string Text { get; set; }
+        public Reviewer Reviewer { get; set; }
+        public Pokemon Pokemon { get; set; }
     }
 }
--- webAPI-ASP/Model/Reviewer.cs
+++ webAPI-ASP/Model/Reviewer.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public string Firstname { get; set; }
-
         public string Lastname { get; set; }
+
+        //ICollection is a list, but can't be edited
+        public ICollection<Review> Reviews { get; set; }
     }
 }
