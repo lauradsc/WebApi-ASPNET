2d903220418f359229604cf4bb7a7a2d25f3af15
LAURA DE SOUZA COSTA <p811916@d02.prodemge.gov.br>
2023-11-30 11:28:03 -0300

    add: still completing the model

--- webAPI-ASP/Model/Category.cs
+++ webAPI-ASP/Model/Category.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        //many-to-many relationships
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
     }
 }
